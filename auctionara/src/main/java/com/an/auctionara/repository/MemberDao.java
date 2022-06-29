cfc7621f63936d8c6adf9b3a01163a0f6f06a58f
Gang-Eun <tjfrkd7415@naver.com>
2022-06-29 11:29:32 +0900

    블랙회원 등록 / 취소, 검색 + 페이징 list 구현 중 중단

---
+++ auctionara/src/main/java/com/an/auctionara/repository/MemberDao.java
@@ -0,0 +1,9 @@
+package com.an.auctionara.repository;
+
+import java.util.List;
+
+import com.an.auctionara.entity.MemberDto;
+
+public interface MemberDao {
+
+}
---
+++ auctionara/src/main/java/com/an/auctionara/repository/MemberDaoImpl.java
@@ -0,0 +1,17 @@
+package com.an.auctionara.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.an.auctionara.entity.MemberDto;
+
+@Repository
+public class MemberDaoImpl implements MemberDao{
+
+	@Autowired
+	private SqlSession sqlSession; 
+
+}
