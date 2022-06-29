19c4974cd11d264320ea081b2f9820b524b520e2
Gang-Eun <tjfrkd7415@naver.com>
2022-06-29 16:50:44 +0900

    현금화 관련 UPDATE, SELECE 기능 구현 (진행 중)
    - view 페이지에 list가 출력이 되지 않아 Test 진행 중 Test도 정상적으로 작동하지 않아서 추후에 강사님께 여쭤보고
    다시 진행하기로 결정

---
+++ auctionara/src/main/java/com/an/auctionara/admin/controller/CashingController.java
@@ -0,0 +1,69 @@
+package com.an.auctionara.admin.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.an.auctionara.entity.CashingPointsDto;
+import com.an.auctionara.repository.CashingPointsDao;
+
+
+@Controller
+@RequestMapping("/admin/cashing")
+public class CashingController {
+	
+	@Autowired
+	private CashingPointsDao cashingPointsDao;
+	private CashingPointsDto cashingPointsDto;
+	
+	@GetMapping("/list")
+	public String list(
+			@RequestParam(required = false) String type,
+			@RequestParam(required = false) String keyword,
+			@RequestParam(required = false, defaultValue = "1") int p, // 통신은 문자열 
+			@RequestParam(required = false, defaultValue = "10") int s,
+			Model model) {
+		
+		List<CashingPointsDto> list = cashingPointsDao.list(type, keyword, p, s);
+		model.addAttribute("list", list);
+		
+		
+		boolean search = type != null && keyword != null;
+		model.addAttribute("search", search);
+		
+		int count = cashingPointsDao.count(type, keyword);
+		
+		int lastPage = (count + s -1) / s; 
+		int blockSize = 10; 
+		int endBlock = (p + blockSize -1) / blockSize * blockSize;
+		int startBlock = endBlock - (blockSize -1);
+		
+		if(endBlock > lastPage) {
+			endBlock = lastPage;
+		}
+		
+		model.addAttribute("p", p);
+		model.addAttribute("s", s);
+		model.addAttribute("type", type);
+		model.addAttribute("keyword",keyword);
+		model.addAttribute("startBlock", startBlock);
+		model.addAttribute("endBlock", endBlock);
+		model.addAttribute("lastPage", lastPage);
+		
+		return "admin/cashing/list"; 
+	}
+	
+	@GetMapping("/request_list")
+	public String list(Model model) {
+		List<CashingPointsDto> requestList = cashingPointsDao.requestList();
+		model.addAttribute("reqeustList", requestList);
+		
+		return "admin/cashing/request_list";
+	}
+}
---
+++ auctionara/src/main/java/com/an/auctionara/repository/CashingPointsDaoImpl.java
@@ -0,0 +1,52 @@
+package com.an.auctionara.repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.an.auctionara.entity.CashingPointsDto;
+
+@Repository
+public class CashingPointsDaoImpl implements CashingPointsDao {
+
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@Override
+	public List<CashingPointsDto> list(String type, String keyword, int p, int s) {
+		// 검색 + 페이징 메소드 
+		Map<String, Object> param = new HashMap<String, Object>();
+		param.put("type", type);
+		param.put("keyword", keyword);
+		
+		
+		int end = p * s;
+		int begin = end - (s - 1);
+		
+		param.put("begin", begin);
+		param.put("end", end);
+		
+		
+		return sqlSession.selectList("cashingPoints.list", param);
+	}
+	
+	@Override
+	public int count(String type, String keyword) {
+		// list 페이징용 count 
+		Map<String, Object> param = new HashMap<String, Object>();
+		param.put("type", type);
+		param.put("keyword", keyword);
+		
+		return sqlSession.selectOne("cashingPoints.count", param);
+	}
+	
+	@Override
+	public List<CashingPointsDto> requestList() {
+		// 현금화 신청 list 
+		return sqlSession.selectList("cashingPoints.requestList");
+	}
+}
