71ad74a90daae63be643e881460fb94f52952d0e
Euiseon <inia8inia@gmail.com>
2022-07-04 21:24:52 +0900

    DB에 이메일이 존재하는지 검증

--- auctionara/src/main/java/com/an/auctionara/rest/CertController.java
+++ auctionara/src/main/java/com/an/auctionara/rest/CertController.java
@@ -1,15 +1,15 @@
 package com.an.auctionara.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.an.auctionara.entity.CertDto;
 import com.an.auctionara.repository.CertDao;
+import com.an.auctionara.repository.MemberDao;
 import com.an.auctionara.service.CertService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -23,8 +23,21 @@ public class CertController {
 	
 	@Autowired
 	CertDao certDao;
-			
 	
+	@Autowired
+	MemberDao memberDao;
+			
+	@GetMapping("/emailExists")
+	public boolean emailExists(@RequestParam String certTarget) {
+		boolean memberNo = memberDao.checkMemberNo(certTarget);
+		
+		if (memberNo) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 	
 	@PostMapping("/asyncSend")
 	public void sendCert(@RequestParam String certTarget) {
