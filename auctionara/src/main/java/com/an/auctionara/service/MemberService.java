f19177914f38a3b433d3055c422fc5153df5fa5f
Euiseon <inia8inia@gmail.com>
2022-07-13 11:31:49 +0900

    회원가입 시 프로필 등록 구현

--- auctionara/src/main/java/com/an/auctionara/service/MemberService.java
+++ auctionara/src/main/java/com/an/auctionara/service/MemberService.java
@@ -2,6 +2,8 @@ package com.an.auctionara.service;
 
 import java.io.IOException;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.web.multipart.MultipartFile;
 
 import com.an.auctionara.entity.MemberDto;
@@ -9,6 +11,6 @@ import com.an.auctionara.entity.MemberDto;
 public interface MemberService {
 	
 	//첨부파일(프로필) 없이 구현 후 사용할 것
-	void join(MemberDto memberDto, MultipartFile attachmentNo) throws IllegalStateException, IOException;
+	void join(MemberDto memberDto, MultipartFile memberProfile) throws IllegalStateException, IOException;
 	
 }
--- auctionara/src/main/java/com/an/auctionara/service/MemberServiceImpl.java
+++ auctionara/src/main/java/com/an/auctionara/service/MemberServiceImpl.java
@@ -1,10 +1,8 @@
 package com.an.auctionara.service;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +10,10 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.an.auctionara.entity.MemberDto;
+import com.an.auctionara.repository.AttachmentDao;
 import com.an.auctionara.repository.MemberDao;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 public class MemberServiceImpl implements MemberService {
@@ -20,17 +21,31 @@ public class MemberServiceImpl implements MemberService {
 	@Autowired
 	private MemberDao memberDao;
 	
+	@Autowired
+	private AttachmentDao attachmentDao;
+	
+	
 	
 	//첨부파일(프로필) 없이 구현함 이후 추가 구현 필요
 	@Override
 	@Transactional
-	public void join(MemberDto memberDto, MultipartFile attachmentNo) throws IllegalStateException, IOException {
-		memberDao.join(memberDto);
+	public void join(MemberDto memberDto, MultipartFile attachment) throws IllegalStateException, IOException {
+		
+		System.out.println(attachment);
 		
 		//프로필 등록코드
-		if(!attachmentNo.isEmpty())	{
+		if(!attachment.isEmpty())	{
+			
+			int attachmentNo = attachmentDao.save(attachment);
+			
+			memberDto.setAttachmentNo(attachmentNo);
+	
+			memberDao.join(memberDto);
+			
 			
 		}
+		
+		
 		
 	}
 
