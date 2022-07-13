5b5e7f1cb7ab15d40deb23fd4ecf32de7ba092bc
Daybreak53 <wnghddk123@naver.com>
2022-07-13 11:00:39 +0900

    일부 수정

--- auctionara/src/main/java/com/an/auctionara/controller/AuctionController.java
+++ auctionara/src/main/java/com/an/auctionara/controller/AuctionController.java
@@ -7,8 +7,6 @@ import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -35,7 +33,6 @@ import com.an.auctionara.repository.SuccessfulBidDao;
 import com.an.auctionara.service.AuctionService;
 import com.an.auctionara.vo.AuctionDetailRefreshVO;
 import com.an.auctionara.vo.AuctionDetailVO;
-import com.an.auctionara.vo.AuctionListVO;
 import com.an.auctionara.vo.WriteAuctionVO;
 
 @Controller
