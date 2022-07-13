b8fc81838ba9de9b47caa0e4722cb1ec3826a051
Euiseon <inia8inia@gmail.com>
2022-07-13 11:31:58 +0900

    마이페이지 구현 중

--- auctionara/src/main/java/com/an/auctionara/controller/MypageController.java
+++ auctionara/src/main/java/com/an/auctionara/controller/MypageController.java
@@ -2,22 +2,49 @@ package com.an.auctionara.controller;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.an.auctionara.entity.MemberDto;
+import com.an.auctionara.repository.MemberDao;
+import com.an.auctionara.service.MemberService;
+
 @Controller
 @RequestMapping("/mypage")
 public class MypageController {
 	
+	@Autowired
+	private MemberDao memberDao;
+	
+	@Autowired
+	private MemberService memberService;
+	
+	
 	@GetMapping("/index")
-	public String infoIndex() {
+	public String mypageIndex(HttpSession session, Model model) {
+		int memberNo = (int) session.getAttribute("whoLogin");
+		
+		MemberDto memberDto = memberDao.memberSearch(memberNo);
+		model.addAttribute("memberDto", memberDto);
+		
+		int attachmentNo = memberDto.getAttachmentNo();
+		
+		if(attachmentNo == 0) {
+			model.addAttribute("profileUrl", "/image/user.png");
+		}
+		else {
+			model.addAttribute("profileUrl", "/attachment/download?attachmentNo=" + attachmentNo);
+		}
+		
+		
 		return "mypage/index";
 	}
 	
 	@GetMapping("/info")
-	public String info(HttpSession session, Model Model) {
+	public String info(HttpSession session, Model model) {
 		return "mypage/info";
 	}
 	
@@ -27,17 +54,17 @@ public class MypageController {
 	}
 	
 	@GetMapping("/auction_history")
-	public String auctionHistory(HttpSession session, Model Model) {
+	public String auctionHistory(HttpSession session, Model model) {
 		return "mypage/auction_history";
 	}
 	
 	@GetMapping("/pay_history")
-	public String payHistory(HttpSession session, Model Model) {
+	public String payHistory(HttpSession session, Model model) {
 		return "mypage/pay_history";
 	}
 	
 	@GetMapping("/cash_log")
-	public String cashLog(HttpSession session, Model Model) {
+	public String cashLog(HttpSession session, Model model) {
 		return "mypage/cash_log";
 	}
 	
